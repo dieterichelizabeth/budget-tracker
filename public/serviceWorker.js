c8b36a7e4e0cca9b0b2985820c0710e67d5438ff
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-05-04 14:25:21 -0500

    Removes old cache in Activate Service Worker

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -3,7 +3,7 @@ const version = "v1";
 const CACHE_NAME = appName + version;
 const CACHE_FILES = [
   "./index.html",
-  "./css/style.css",
+  "./css/styles.css",
   "./js/idb.js",
   "./js/index.js",
 ];
@@ -11,6 +11,8 @@ const CACHE_FILES = [
 // Install the Service Worker
 self.addEventListener("install", function (event) {
   // console.log("Service worker is installed!");
+
+  // Add all cache files
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
       return cache.addAll(CACHE_FILES);
@@ -20,5 +22,19 @@ self.addEventListener("install", function (event) {
 
 // Activate the Service Worker
 self.addEventListener("activate", function (event) {
-  console.log("Service worker activated!");
+  // console.log("Service worker activated!");
+
+  // Remove unwanted caches
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function (cache) {
+          if (cache !== CACHE_NAME) {
+            console.log("Cleaning Cache");
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
 });
