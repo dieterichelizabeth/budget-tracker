d80ff0c03bba92e0c0f7fd3cf944c5f5af2cf1e2
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-05-04 12:53:37 -0500

    Add cache to Service Worker installation

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -10,7 +10,12 @@ const CACHE_FILES = [
 
 // Install the Service Worker
 self.addEventListener("install", function (event) {
-  console.log("Service worker is installed!");
+  // console.log("Service worker is installed!");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(CACHE_FILES);
+    })
+  );
 });
 
 // Activate the Service Worker
